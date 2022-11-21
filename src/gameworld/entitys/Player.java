2fb86e91bb8505b69dfb982bd967826feb3ff77b
gk646 <lukas.gilch@gmx.de>
2022-11-21 18:57:09 +0100

    working on projectile collision

--- src/gameworld/entitys/Player.java
+++ src/gameworld/entitys/Player.java
@@ -48,7 +48,7 @@ public class Player extends Entity {
         Utilities utilities = new Utilities();
         BufferedImage scaledImage = null;
         try {
-            scaledImage = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/resources//player/" + imagePath)));
+            scaledImage = ImageIO.read((getClass().getResourceAsStream("/resources/player/" + imagePath)));
             scaledImage = utilities.scaleImage(scaledImage, 48, 48);
 
         } catch (IOException e) {
