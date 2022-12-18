757a8e9402138f56b2d0126c96c6371888947153
gk656 <lukas.gilch@gmx.de>
2022-12-18 16:39:37 +0100

    inventory progress

--- src/main/system/ui/InventoryPanel.java
+++ src/main/system/ui/InventoryPanel.java
@@ -19,11 +19,13 @@ public class InventoryPanel extends JPanel {
     public final Color lightBackground = new Color(192, 203, 220), lightBackgroundAlpha = new Color(0xCAC0CBDC, true), darkBackground = new Color(90, 105, 136);
     private final int selectedSlot;
     private final int draggedSlot;
-    private final Item[] bag_Slots;
+    private final InventorySlot[] bag_Slots;
     public MainGame mg;
     MouseMotionListener motionL;
     MouseListener mouseL;
-    public Item[] char_Slots;
+    public InventorySlot[] char_Slots;
+    Item grabbedItem;
+    InventorySlot grabbedSlot;
     private int dragOffsetX;
     private int dragOffsetY;
 
@@ -32,33 +34,67 @@ public class InventoryPanel extends JPanel {
         this.setSize(new Dimension(1000, 750));
         setBackground(new Color(192, 203, 220, 255));
         //todo panel inside panel
-        addMouseListener(mouseL);
-        addMouseMotionListener(motionL);
-        setVisible(true);
         this.mg = mainGame;
         this.motionL = mg.motionHandler;
         this.mouseL = mg.mouseHandler;
-        bag_Slots = new Item[BAG_SLOTS];
-        char_Slots = new Item[10];
+        addMouseListener(mouseL);
+        addMouseMotionListener(motionL);
+        setVisible(true);
+
+        bag_Slots = new InventorySlot[BAG_SLOTS];
+        char_Slots = new InventorySlot[10];
         selectedSlot = -1;
         draggedSlot = -1;
-        char_Slots[1] = new ARM_chest01();
-
+        this.grabbedItem = null;
+        createCharSlots(260, 200);
+        char_Slots[1].setItem(char_Slots[1], new ARM_chest01());
     }
 
 
     public void draw(Graphics2D g2) {
         g2.setFont(mg.ui.maruMonica);
-        drawDragandDrop();
+
         drawInventoryWindow(g2);
+        drawDragandDrop();
         drawCharacterSlots(g2, 260, 200);
-
     }
 
 
-    public void drawDragandDrop() {
+    public int drawDragandDrop() {
+        for (InventorySlot invSlot : char_Slots) {
+            if (invSlot.grabbed) {
+                return 0;
+            }
+        }
+        if (grabbedItem == null && mg.mouseHandler.mouse1Pressed) {
+            for (InventorySlot invSlot : char_Slots) {
+                if (invSlot.boundBox.contains(mg.mouseHandler.mouse1Position) && invSlot.item != null) {
+                    invSlot.grabbed = true;
+                    grabbedSlot = invSlot;
+                    grabbedItem = invSlot.item;
+                    System.out.println("grabbed");
+                }
+            }
+        }
+        if (grabbedItem != null && !mg.mouseHandler.mouse1Pressed) {
+            System.out.println("set");
+            for (InventorySlot invSlot : char_Slots) {
+                if (invSlot.boundBox.contains(mg.motionHandler.lastMousePosition)) {
+                    invSlot.setItem(grabbedSlot, grabbedItem);
+                    grabbedSlot.item = null;
+                    grabbedItem = null;
+                    grabbedSlot = null;
 
+                } else {
+                    System.out.println("reset");
+                    grabbedSlot.grabbed = false;
+                    grabbedItem = null;
+                }
+            }
+        }
+        return 1;
     }
+
 
     public void drawCharacterSlots(Graphics2D g2, int startX, int startY) {
         g2.setColor(darkBackground);
@@ -66,80 +102,20 @@ public class InventoryPanel extends JPanel {
         g2.setColor(darkBackground);
         g2.drawRoundRect(100 + startX, startY + 90, 200, 250, 25, 25);
         g2.drawImage(mg.player.entityImage1, startX + 120, startY + 150, 150, 150, null);
-        //LEFT
-        g2.drawRoundRect(40 + startX, 130 + startY, SLOT_SIZE, SLOT_SIZE, 20, 20);
-        g2.drawRoundRect(40 + startX, 130 + 50 + startY, SLOT_SIZE, SLOT_SIZE, 20, 20);
-        g2.drawRoundRect(40 + startX, 130 + 100 + startY, SLOT_SIZE, SLOT_SIZE, 20, 20);
-        g2.drawRoundRect(40 + startX, 130 + 150 + startY, SLOT_SIZE, SLOT_SIZE, 20, 20);
-        //RIGHT
-        g2.drawRoundRect(40 + 270 + startX, 130 + startY, SLOT_SIZE, SLOT_SIZE, 20, 20);
-        g2.drawRoundRect(40 + 270 + startX, 130 + 50 + startY, SLOT_SIZE, SLOT_SIZE, 20, 20);
-        g2.drawRoundRect(40 + 270 + startX, 130 + 100 + startY, SLOT_SIZE, SLOT_SIZE, 20, 20);
-        g2.drawRoundRect(40 + 270 + startX, 130 + 150 + startY, SLOT_SIZE, SLOT_SIZE, 20, 20);
-        //Below
-        g2.drawRoundRect(150 + startX, 110 + 240 + startY, SLOT_SIZE, SLOT_SIZE, 20, 20);
-        g2.drawRoundRect(50 + 150 + startX, 110 + 240 + startY, SLOT_SIZE, SLOT_SIZE, 20, 20);
-
+        for (InventorySlot invSlot : char_Slots) {
+            invSlot.drawSlot(g2);
+            if (invSlot.item != null && invSlot.grabbed) {
+                invSlot.drawIcon(g2, mg.motionHandler.lastMousePosition.x - SLOT_SIZE / 2, mg.motionHandler.lastMousePosition.y - SLOT_SIZE / 2, SLOT_SIZE);
+            } else if (invSlot.item != null) {
+                invSlot.drawIcon(g2, invSlot.xCo, invSlot.yCo, SLOT_SIZE);
+            }
+        }
         //Stats Text
         g2.setColor(darkBackground);
         g2.setFont(g2.getFont().deriveFont(Font.BOLD, 30f));
         g2.drawString("Mage: Level" + mg.player.level, 120 + startX, 40 + startY);
         g2.setFont(g2.getFont().deriveFont(Font.BOLD, 15f));
         g2.drawString("Health:" + mg.player.maxHealth, startX + 110, startY + 420);
-        //Icons
-        drawCharIcons(g2, startX, startY);
-    }
-
-    public void drawCharIcons(Graphics2D g2, int startX, int startY) {
-        //------------------------Left Side
-        for (int i = 0; i <= 3; i++) {
-            int x = startX + 40;
-            int y = (i * (5 + SLOT_SIZE)) + startY + 130;
-            /*
-            if (i == selectedSlot) {
-                g2.setColor(Color.LIGHT_GRAY);
-                g2.fillRect(x, y, SLOT_SIZE, SLOT_SIZE);
-            }
-             */
-            if (char_Slots[i] != null) {
-                char_Slots[i].drawIcon(g2, x, y, SLOT_SIZE);
-            }
-        }
-        //--------------------Right Side
-        for (int i = 4; i <= 7; i++) {
-            int x = 40 + 270 + startX;
-            int y = ((i - 4) * (5 + SLOT_SIZE)) + 130 + startY;
-            /*
-            if (i == selectedSlot) {
-                g2.setColor(Color.LIGHT_GRAY);
-                g2.fillRect(x, y, SLOT_SIZE, SLOT_SIZE);
-            }
-             */
-            if (char_Slots[i] != null) {
-                char_Slots[i].drawIcon(g2, x, y, SLOT_SIZE);
-            }
-        }
-        //-------------------Below
-        for (int i = 8; i <= 9; i++) {
-            int x = ((i - 8) * (SLOT_SIZE + 5)) + 150 + startX;
-            int y = 110 + 240 + startY;
-            /*
-            if (i == selectedSlot) {
-                g2.setColor(Color.LIGHT_GRAY);
-                g2.fillRect(x, y, SLOT_SIZE, SLOT_SIZE);
-            }
-             */
-            if (char_Slots[i] != null) {
-                char_Slots[i].drawIcon(g2, x, y, SLOT_SIZE);
-            }
-        }
-        if (draggedSlot != -1) {
-            int x = draggedSlot * SLOT_SIZE;
-            int y = 0;
-            Item item = char_Slots[draggedSlot];
-            item.drawIcon(g2, x + dragOffsetX, y + dragOffsetY, SLOT_SIZE);
-        }
-
     }
 
     public void drawInventoryWindow(Graphics2D g2) {
@@ -147,7 +123,7 @@ public class InventoryPanel extends JPanel {
         g2.fillRoundRect(210, 135, 1500, 800, 50, 50);
     }
 
-    public void drawBags(Graphics2D g2) {
+    public void drawBag(Graphics2D g2) {
         for (int i = 0; i < BAG_SLOTS; i++) {
             int x = i * SLOT_SIZE + 250;
             int y = 250;
@@ -162,8 +138,20 @@ public class InventoryPanel extends JPanel {
         if (draggedSlot != -1) {
             int x = draggedSlot * SLOT_SIZE;
             int y = 0;
-            Item item = char_Slots[draggedSlot];
-            item.drawIcon(g2, x + dragOffsetX, y + dragOffsetY, SLOT_SIZE);
+            // Item item = char_Slots[draggedSlot];
+            // item.drawIcon(g2, x + dragOffsetX, y + dragOffsetY, SLOT_SIZE);
+        }
+    }
+
+    private void createCharSlots(int startX, int startY) {
+        for (int i = 0; i <= 3; i++) {
+            char_Slots[i] = new InventorySlot(null, 40 + startX, (i * 50) + 130 + startY);
+        }
+        for (int i = 4; i <= 7; i++) {
+            char_Slots[i] = new InventorySlot(null, 40 + 270 + startX, ((i - 4) * 50) + 130 + startY);
+        }
+        for (int i = 8; i <= 9; i++) {
+            char_Slots[i] = new InventorySlot(null, ((i - 8) * 50) + 40 + startX, 110 + 240 + startY);
         }
     }
 }
--- src/main/system/ui/InventorySlot.java
+++ src/main/system/ui/InventorySlot.java
@@ -2,9 +2,34 @@ package main.system.ui;
 
 import gameworld.Item;
 
-public class InventorySlot {
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
 
+public class InventorySlot {
+    private final int SLOT_SIZE = 45;
+    public boolean grabbed;
+    int xCo;
+    int yCo;
+    Rectangle boundBox;
     Item item;
 
+    InventorySlot(Item item, int xCo, int yCo) {
+        this.boundBox = new Rectangle(xCo, yCo, SLOT_SIZE, SLOT_SIZE);
+        this.item = item;
+        this.xCo = xCo;
+        this.yCo = yCo;
+    }
 
+    public void setItem(InventorySlot invSlot, Item item) {
+        invSlot.item = item;
+    }
+
+    public void drawIcon(Graphics2D g2, int x, int y, int slotSize) {
+        item.drawIcon(g2, x, y, slotSize);
+    }
+
+    public void drawSlot(Graphics2D g2) {
+        g2.drawRoundRect(this.xCo, this.yCo, SLOT_SIZE, SLOT_SIZE, 20, 20);
+
+    }
 }
