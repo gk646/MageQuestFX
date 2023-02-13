489386251b3dc047b9e6e9d2df1e2e4c6e43ddeb
gk646 <lukas.gilch@gmx.de>
2023-02-13 22:02:21 +0100

    tried better graphics

---
+++ TiledMaps/tileset16.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="grassspritesheet16" tilewidth="16" tileheight="16" tilecount="225" columns="15">
+ <image source="grassspritesheet16.png" width="240" height="240"/>
+</tileset>
