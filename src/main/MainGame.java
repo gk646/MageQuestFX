2253d99f2bf461ceb1d9752a67354711f097a20e
gk646 <lukas.gilch@gmx.de>
2023-03-11 17:35:06 +0100

    boss updates

--- src/main/MainGame.java
+++ src/main/MainGame.java
@@ -456,7 +456,7 @@ public class MainGame {
             sound.INTRO.setCycleCount(MediaPlayer.INDEFINITE);
             sound.INTRO.play();
         }
-        //debug();
+        debug();
     }
 
     private void debug() {
