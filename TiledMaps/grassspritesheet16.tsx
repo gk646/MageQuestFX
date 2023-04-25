4edfc74729887117bd8f773c3dfd93b65106decb
gk646 <lukas.gilch@gmx.de>
2023-04-25 20:51:18 +0200

    delete commit

--- TiledMaps/grassspritesheet16.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="grassspritesheet16" tilewidth="16" tileheight="16" tilecount="896" columns="13">
- <image source="grassspritesheet16.png" width="222" height="1024"/>
- <tile id="40">
-  <properties>
-   <property name="s" value=""/>
-  </properties>
- </tile>
-</tileset>
